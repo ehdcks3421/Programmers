f5d377a9d006189d5203a05a5b018118ebfb3a65
ehdcks3421 <ehdcks3421@naver.com>
2021-02-23 18:13:23 +0900

    re

---
+++ 두개 뽑아서 더하기_level1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution { 
+    public ArrayList<Integer> solution(int[] numbers) {
+        ArrayList<Integer> list=new ArrayList<>();
+        HashSet<Integer> hash=new HashSet<Integer>();
+        
+        
+        for(int i=0; i<numbers.length-1; i++)
+        {
+            for(int j=i+1; j<numbers.length; j++)
+            {
+                hash.add(numbers[i]+numbers[j]);
+            }
+        }
+        
+        Iterator<Integer> iter=hash.iterator();
+        while(iter.hasNext())
+        {
+            list.add(iter.next());
+        }
+        
+        Collections.sort(list);
+        
+        
+        return list;
+        
+    }
+}
