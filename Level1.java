d7ab79e0d2c9474d90d48d339267da24e6914532
ehdcks3421 <ehdcks3421@naver.com>
2021-02-26 15:09:11 +0900

    스택문제

--- Level1.java
+++ Level1.java
@@ -1,39 +1,49 @@
 import java.util.*;
 
 public class Level1 {
+	static ArrayList<Integer> list=new ArrayList<Integer>();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] number= {1,2,3,4,5};
-	
-		System.out.println(solution(number));
+		int[][] board= {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+		int[] moves={1,5,3,5,1,2,1,4};
+		
+		System.out.println(solution(board, moves));
 	}
 	
-
-	    public static ArrayList<Integer> solution(int[] numbers) {
-	        ArrayList<Integer> list=new ArrayList<>();
-	        HashSet<Integer> hash=new HashSet<Integer>();
+	    public static int solution(int[][] board, int[] moves) {
+	    	
+	        int answer = 0;
 	        
-	        
-	        for(int i=0; i<numbers.length-1; i++)
+	        for(int i=0; i<moves.length; i++)
 	        {
-	            for(int j=i+1; j<numbers.length; j++)
-	            {
-	                hash.add(numbers[i]+numbers[j]);
-	            }
+	        	for(int j=0; j<board.length; j++)
+	        	{
+	        		if(board[j][moves[i]-1]!=0)
+	        		{
+	        			list.add(board[j][moves[i]-1]);
+	        			board[j][moves[i]-1]=0;
+	        			break;
+	        		}	
+	        	}	
 	        }
 	        
-	        Iterator<Integer> iter=hash.iterator();
-	        while(iter.hasNext())
+	        for(int k=0; k<list.size(); k++)
 	        {
-	            list.add(iter.next());
+	        	if(k==list.size()-1)
+	        		break;
+	        	else if(list.get(k)==list.get(k+1))
+	        	{
+	        		list.remove(k);
+	        		list.remove(k);
+	        		answer+=2;	        		
+	        		k=-1;
+	        		
+	        	}
 	        }
 	        
-	        Collections.sort(list);
-	        
-	        
-	        return list;
-	        
+	        return answer;
 	    }
 	}
+	
 
 
