986641866145baa0b8208830b21229df25207bea
ehdcks3421 <ehdcks3421@naver.com>
2021-03-22 14:47:10 +0900

    n/2

---
+++ 약수의 합.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Level1 {
+	public static void main(String[] args) {	
+		int n=5;
+		System.out.println(solution(n));
+	}
+    public static int solution(int n) {
+        int answer = 0;
+
+        for(int i=1; i<=(n/2); i++)
+        {
+        	if(n%i==0) answer+=i;
+        }
+        
+        return answer+n;
+    }
+
+	   
+}
+	
+	
+
+
